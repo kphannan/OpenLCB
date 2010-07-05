46fd116e1a04cf233612cf4362178d7e135978a6
Bob Jacobsen <jacobsen@mac.com>
2010-07-05 17:29:42 +0000

    split to separate .cpp, hoist duplicate if branches

---
+++ prototypes/Arduino/libraries/OpenLCB/ButtonLed.cpp
@@ -0,0 +1,63 @@
+#include "WConstants.h"
+#include "ButtonLed.h"
+
+
+ButtonLed::ButtonLed(uint8_t p) : pin(p) {
+  debounce=50;
+  next = true;
+}
+
+void ButtonLed::on(long mask) {
+  pattern = mask;
+  ledState=HIGH;  // turns LED off
+  if( (pattern&0x1) != 0) ledState=LOW;  // turns LED on
+  next = true;
+  pinMode(pin,OUTPUT);
+  digitalWrite(pin,ledState); // initialize
+}
+
+void ButtonLed::process() {
+  int s;
+  sample+=1; 
+  if(sample>10) {           // sample button every 10 calls
+    sample=0;               // reset cycle counter
+
+    // do input cycle
+    pinMode(pin, INPUT);
+    digitalWrite(pin,HIGH); // activate pull up
+    s=digitalRead(pin);
+    
+    // set pin back to output
+    pinMode(pin,OUTPUT);
+    digitalWrite(pin,ledState); 
+    
+    // do state change and debounce logic
+    if (waiting && ((millis()-lastTime)>debounce)) {
+        // waiting for debounce && debounce complete
+        waiting = false;
+        state = s;
+    } else if (s!=state) {
+        // start of change
+        lastDuration = duration;
+        lastTime = millis();
+        waiting = true;
+    }
+  }
+  // drive output pattern
+  if ( next && (millis()&0x3F) == 0) {
+    // time for next bit
+    if ((pattern & 0x1) !=0) {
+       ledState = LOW;
+       pattern = 0x80000000 | (pattern>>1);
+    } else {
+       ledState = HIGH;
+       pattern = 0x7FFFFFFF & (pattern>>1);
+    }
+    digitalWrite(pin,ledState);
+    next = false;
+  } else {
+    // wait for next bit
+    if ( (millis()&0x3F) != 0) next = true;
+  }
+}
+
--- prototypes/Arduino/libraries/OpenLCB/ButtonLed.h
+++ prototypes/Arduino/libraries/OpenLCB/ButtonLed.h
@@ -1,11 +1,17 @@
+#ifndef ButtonLed_h
+#define ButtonLed_h
+
 // ButtonLED class
+// By David Harris   Copyright 2010
 //
-// Define a button: buttonLed b0(pin) or b0(pin,LOW) or b0(pin,HIGH)
-// Periodically call b0.process(), it will return true if the button is down, else false
-// b0.duration will return how long the button has been in its current state in ms
-// b0.lastDuration will return how long it was in the last state, in msec
+// Define a button: buttonLed b0(pin)
+// Periodically call .process()
 //
-// b0.on(pattern) will drive the LED from the given pattern, LSB first
+// .state is the current state, as would-be read from the pin
+// .duration is how long the button has been in its current state in msec
+// .lastDuration is how long it was in the previous state, in msec
+//
+// .on(pattern) will drive the LED from the given pattern, LSB first
 //     0 bits in pattern are off, 1 bits are on
 //     each bit shows for 64 msec, aligned across all ButtonLed instances
 //  full off:             0x0L
@@ -16,7 +22,7 @@
 //  double blink:  0x00000033L
 //  wink:          0xFFFFFFFEL
 
-// Hardware is:
+// Hardware is e.g:
 //  +5V---R1---LED>|----pin----R2----button---gnd
 // where R1 is 0.5-1k, R2 is >0.5k.
 // Values of 500 and 1k work well for cheap LEDs. 
@@ -26,91 +32,24 @@
 
 class ButtonLed {
   private:
-    long lastTime;
-    bool down;
-    int sample;
-    bool next;
+    long lastTime;            // time of last change
+    int sample;               // counter for read checks
+    bool next;                // wait-next-timeslot flag in output loop
+    bool waiting;             // true if waiting for debounce
   public:
-    uint8_t pin;
-    bool ledState;
-    long debounce;
-    int sense; // LOW=DOWN, HIGH=UP
-    long duration;
-    long lastDuration;
-    long pattern;
-
-    ButtonLed(uint8_t p) {
-      pin=p;
-      debounce=50;
-      sense=HIGH;
-      next = true;
-    }
-    ButtonLed(uint8_t p,int s) {
-      pin=p;
-      sense=s;
-      debounce=50;
-      next = true;
-    }
-    void on(long mask) {
-      pattern = mask;
-      ledState=HIGH;  // turns LED off
-      if( (pattern&0x1) != 0) ledState=LOW;  // turns LED on
-      next = true;
-      pinMode(pin,OUTPUT);
-      digitalWrite(pin,ledState); // initialize
-    }
-    bool process() {
-      int s;
-      sample+=1; 
-      if(sample>10) {          // sample button every 100 processes
-        pinMode(pin, INPUT);    // so input
-        digitalWrite(pin,HIGH); // activate pull up
-        sample=0;               // reset cycle counter
-        duration=millis()-lastTime;
-        if(down) {  // button was down
-          if(duration>debounce) { // definitely down
-             down=true;
-          }
-          s=digitalRead(pin);
-          if(s!=sense) {       // button changed to up
-            lastDuration=duration;
-            duration=0;
-            down=false;        // tag it
-            lastTime=millis(); // start timing
-          } 
-        } else { // button was up
-          if(duration>debounce) { // definitely up
-             down=false;
-          }
-          s=digitalRead(pin);
-          if(s==sense) {       // button changed to down
-            lastDuration=duration;
-            duration=0;
-            down=true;         // remember
-            lastTime=millis(); // start timing
-          }
-        }
-        // and set back to output
-        pinMode(pin,OUTPUT);
-        digitalWrite(pin,ledState); 
-      }
-      if ( next && (millis()&0x3F) == 0) {
-        if ((pattern & 0x1) !=0) {
-           ledState = LOW;
-           pattern = 0x80000000 | (pattern>>1);
-        } else {
-           ledState = HIGH;
-           pattern = 0x7FFFFFFF & (pattern>>1);
-        }
-        digitalWrite(pin,ledState);
-        next = false;
-      } else {
-        if ( (millis()&0x3F) != 0) next = true;
-      }
-      return down;
-    }
+    long debounce;            // debounce interval
+    long pattern;             // current output drive pattern (rotates)
+    long duration;            // how long the button has been in its current state in msec
+    long lastDuration;        // how long it was in the previous state, in msec
+    const uint8_t pin;        // connected pin number
+    bool ledState;            // current output drive state
+    bool state;               // current button state
+    
+    ButtonLed(uint8_t pin);
+    void on(long mask);       // set the output mask
+    void process();           // call periodically
 };
 
-        
+#endif     
 
 
