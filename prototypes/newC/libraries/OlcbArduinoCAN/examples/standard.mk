1f4fbcbd35186d046d3540f09daa99d7e4608f2c
Bob Jacobsen <jacobsen@mac.com>
2010-08-01 23:50:48 +0000

    new organization of library

---
+++ prototypes/newC/libraries/OlcbArduinoCAN/examples/standard.mk
@@ -0,0 +1,33 @@
+# Simple makefile fragment for Arduino sketch, somewhat brute force.
+
+# Arduino install definitions, for Arduino 18
+# The ARDUINO_ROOT definition varies with install location
+ARDUINO_ROOT := /Applications/Arduino.app/Contents/Resources/Java/
+CC_OPTIONS_ARDUINO := -Os -w -fno-exceptions -ffunction-sections -fdata-sections -std=gnu99 -mmcu=atmega168 -DF_CPU=16000000L
+
+INCLUDE_ARDUINO := -I${ARDUINO_ROOT}hardware/arduino/cores/arduino 
+
+INCLUDE_ARDUINO := ${INCLUDE_ARDUINO} -I${ARDUINO_ROOT}libraries/EEPROM
+INCLUDE_ARDUINO := ${INCLUDE_ARDUINO} -I${ARDUINO_ROOT}libraries/LiquidCrystal
+INCLUDE_ARDUINO := ${INCLUDE_ARDUINO} -I${ARDUINO_ROOT}libraries/Servo
+
+# note that this in meant to be included one level down, where PWD refers to a specific examples/sketch
+
+# local libraries
+INCLUDE_OPTIONS := -I${PWD}/../../OlcbArduinoCAN 
+INCLUDE_OPTIONS := ${INCLUDE_OPTIONS} -I${PWD}/../../OlcbCommonCAN 
+INCLUDE_OPTIONS := ${INCLUDE_OPTIONS} -I${PWD}/../../CAN 
+INCLUDE_OPTIONS := ${INCLUDE_OPTIONS} -I${PWD}/../../EtherNet2 
+INCLUDE_OPTIONS := ${INCLUDE_OPTIONS} -I${PWD}/../../LCmini
+INCLUDE_OPTIONS := ${INCLUDE_OPTIONS} -I${PWD}/../../LocoNet
+INCLUDE_OPTIONS := ${INCLUDE_OPTIONS} -I${PWD}/../../NmraDcc
+
+TMPDIR := $(shell mktemp -d /tmp/Arduino.XXXXXXXX)
+
+PDE := $(foreach V,$(wildcard *.pde),$(V:.pde=))
+
+all:
+	cp ${PDE}.pde ${TMPDIR}/${PDE}.cpp
+	-cp *.h ${TMPDIR}/ >& /dev/null
+	cd ${TMPDIR}; ${ARDUINO_ROOT}hardware/tools/avr/bin/avr-g++ -c -g  ${CC_OPTIONS_ARDUINO} ${INCLUDE_ARDUINO} ${INCLUDE_OPTIONS} *.cpp -o /dev/null
+	rm -r ${TMPDIR}
