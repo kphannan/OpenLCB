b329182bb3ce36565b1ecea68fddd7b635731ec1
Bob Jacobsen <jacobsen@mac.com>
2011-06-06 06:16:49 +0000

    fill in header

--- prototypes/iOS/Monitor/Monitor/MonitorAppDelegate.m
+++ prototypes/iOS/Monitor/Monitor/MonitorAppDelegate.m
@@ -3,7 +3,7 @@
 //  Monitor
 //
 //  Created by Bob Jacobsen on 5/16/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Bob Jacobsen. All rights reserved.
 //
 
 #import "MonitorAppDelegate.h"
