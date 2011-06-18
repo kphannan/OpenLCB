6c7055318582ab81f382c930bafb5163e2020552
Bob Jacobsen <jacobsen@mac.com>
2011-06-18 07:08:10 +0000

    builds with device

---
+++ prototypes/ObjectiveC/OpenLcbLib/FlipsideViewController.m
@@ -0,0 +1,63 @@
+//
+//  FlipsideViewController.m
+//  Monitor
+//
+//  Created by Bob Jacobsen on 5/16/11.
+//  Copyright 2011 Bob Jacobsen. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+
+@implementation FlipsideViewController
+
+@synthesize delegate=_delegate;
+@synthesize hostAddress=_hostAddress;
+@synthesize portNumber=_portNumber;
+
+- (void)dealloc
+{
+    [super dealloc];
+    [self->_hostAddress release];
+    [self->_portNumber release];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc. that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor viewFlipsideBackgroundColor];  
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+    self.hostAddress = nil;
+    self.portNumber = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Actions
+
+- (IBAction)done:(id)sender
+{
+    [self.delegate flipsideViewControllerDidFinish:self];
+}
+
+@end
